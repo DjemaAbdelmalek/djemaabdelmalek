9295c7d28005c4d61d29b856f5918a3a13f8984c
DjemaAbdelmalek <mr.djemaabdelmalek@gmail.com>
2025-06-24 14:07:31 +0100

    second commit

--- resources/views/components/sections/contact.blade.php
+++ resources/views/components/sections/contact.blade.php
@@ -1,4 +1,4 @@
 <div class="w-full h-screen bg-background autoscroll-section flex flex-col items-center justify-center px-6">
-    <h1 class="font-semibold text-2xl uppercase cursor-copy md:text-6xl" title="click to copy">
-        mr.djemaabdelmalek@gmail.com</h1>
+    <h1 class="font-semibold text-2xl uppercase cursor-copy md:text-8xl text-center" title="click to copy">
+        mr.djemaabdelmalek<br />@gmail.com</h1>
 </div>
